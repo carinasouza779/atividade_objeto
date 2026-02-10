15f3742c28e5381c75bde35984df5c3028c02c5b
Carina Cristina Teixeira De Souza <carinacristina1809@gmail.com>
2026-02-10 15:52:45 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,80 @@
+//Atividade 01
+let sistemaLogin = {
+    login:"admistrador",
+    senha: 123,
+    tipo: "admin"
+};
+console.log(`O login do ${sistemaLogin.login} é ${istemaLogin.tipo} é ${istemaLogin.senha}, muito fraca por sinal.`)
+
+//Atividade 02
+let cadastro = {
+    nome: "Lucas",
+    curso: "Desenvolvimento de Sistemas ",
+    turma: "2° DS.",
+};
+console.log(`O aluno ${cadastro.nome} está natriculado no curso de ${cadastro.curso} na turma ${cadastro.turma}.`)
+
+//Atividade 03
+let professor = {
+    nome: "Eduardo",
+    disciplina: "JavaScript",
+    turno: "Manha"
+};
+console.log(`O professor ${professor.nome}  ministra a disciplina de  ${professor.disciplina} no turno da ${professor.turno}.`)
+
+//Atividade 04
+let empresa = {
+    nome:"Tech Solutions",
+    area: "Tecnologia",
+    cidade: "Fortaleza.",
+};
+console.log(`A empresa ${empresa.nome}  atua na área de  ${empresa.area} e fica em ${empresa.cidade}.`)
+
+//Atividade 05
+let compurtador = {
+    marca:"Dell",
+    processador: "Intel i5",
+    memoria: "8GB",
+};
+console.log(` O computador da marca  ${compurtador.marca}  O computador da marca   ${compurtador.processador} e memória de ${compurtador.memoria}.`)
+
+//Atividade 06
+let livro = {
+    titulo:"JavaScript Básic",
+    autor: "Ana Silva",
+    paginas: 200,
+};
+console.log(`O livro ${livro.titulo}  foi escrito por  ${livro.autor} e possui  ${livro.paginas} páginas.`)
+
+//Atividade 07
+let  filme = {
+    nome:" Matrix",
+    genero:"Ficção",
+    ano: 1999,
+};
+console.log(` O filme ${filme.nome}   é do gênero  ${filme.genero}e foi lançado em ${filme.ano}.`)
+
+
+//Atividade 08
+let jogo = {
+    nome:"Minecraft",
+    plataforma:"PC",
+    genero: "Sandbox"
+};
+console.log(`O jogo ${jogo.nome} roda na plataforma  ${jogo.plataforma}  e pertence ao gênero ${jogo.genero}.`)
+
+//Atividade 09
+let contaUsuario= {
+    usuario:"eduardo",
+    senha: 1234,
+    nivel: "administrador"
+};
+console.log(`O usuário ${contaUsuario.usuario}  possui nível  ${contaUsuario.nivel} e utiliza a ${contaUsuario.sistemaLogin} muito fraca por sinal.`)
+
+//Atividade 10
+let escola = {
+    nome:"EEEP",
+    cidade:"Fortaleza",
+    turno: "manhã.",
+};
+console.log(` A escola ${escola.nome}  funciona na cidade de  ${escola.cidade} no turno da ${escola.turno}.`)
